9021c150523d2d557b55eb6a449bce97b2a33e4c
Alexander Juarez <couatli@gmail.com>
2014-04-16 23:42:14 -0700

    added dynamic image routing and tagging. Adding a image parser to make the thumbnails smaller.

--- project.clj
+++ project.clj
@@ -3,26 +3,25 @@
   "0.1.0-SNAPSHOT"
   :dependencies
   [[org.clojure/clojure "1.5.1"]
-   [lib-noir "0.8.1"]
+   [lib-noir "0.8.1"];;for io and session utils
    [compojure "1.1.5"]
    [ring-server "0.3.0"]
-   [selmer "0.5.9"]
-   [com.taoensso/timbre "3.1.4"]
-   [com.postspectacular/rotor "0.1.0"]
-   [clojurewerkz/spyglass "1.1.0"]
+   [selmer "0.5.9"];;templating engine
+   [image-resizer "0.1.6"]
+   [com.taoensso/timbre "3.1.4"];;logging
+   [com.postspectacular/rotor "0.1.0"];;loggin
+   [clojurewerkz/spyglass "1.1.0"];;couchbase interface
    [com.taoensso/tower "2.0.2"];;localization lib
-   [markdown-clj "0.9.41"]
-   [hashids "0.1.0"]
-   [clj-time "0.6.0"]
-   [clj-http "0.7.8"]
+   [markdown-clj "0.9.41"];;markdown parser
+   [hashids "0.1.0"];;for anon hashing
+   [clj-http "0.7.8"];;for reading bitcoins prices from coinbase.com
    [net.sf.jlue/jlue-core "1.3"];;why?
    [postgresql/postgresql "9.1-901.jdbc4"]
    [ring-anti-forgery "0.3.0"]
-   [metis "0.3.3"]
-   [korma "0.3.0-RC6"]
+   [metis "0.3.3"];;validator
+   [korma "0.3.0-RC6"];;dbl
    [lobos "1.0.0-beta1"]
    [org.bouncycastle/bcpg-jdk15on "1.50"]
-   ;;[cheshire "5.2.0"]
    [log4j
     "1.2.17"
     :exclusions
---
+++ src/whitecity/views/tags.clj
@@ -0,0 +1,34 @@
+(ns whitecity.views.tags
+  (:require [selmer.parser :refer [add-tag!]]
+            [clojure.java.io :as io]
+            [noir.io :as noirio]
+            [ring.util.anti-forgery :refer [anti-forgery-field]])
+  (:use selmer.filters
+        hiccup.core)
+  (:import 
+    (org.apache.commons.io IOUtils)
+    (org.apache.commons.codec.binary Base64)))
+
+;;this means that we will use a data;mime type to embed the image instead
+(def img-data true)
+
+(defn parse-args [args]
+  (into [] (map keyword (.split args "\\."))))
+
+(defn computed-args [args context-map]
+  (map #(get-in context-map (parse-args %)) args))
+
+(defn read-image [id]
+  (let [path (str (noirio/resource-path) "/uploads/" id ".jpg")]
+    (with-open [in (io/input-stream (io/file path))]
+      (.toString (Base64/encodeBase64String (IOUtils/toByteArray in))))))
+
+(add-tag! :csrf-token (fn [_ _] (anti-forgery-field)))
+
+(add-tag! :image (fn [args context-map] 
+                   (let [id (first (computed-args args context-map))]
+                   (if img-data
+                     (let [data (str "data:image/jpeg;base64," (read-image id))]
+                       (html [:img {:src data}]))
+                     (let [url (str "/uploads/" id ".jpg")]
+                       (html [:img {:src url}]))))))
