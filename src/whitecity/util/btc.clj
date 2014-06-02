de791dc5743293d94335c1542a642b9b0f376da7
Alexander Juarez <couatli@gmail.com>
2014-06-01 22:08:58 -0700

    fixed address validation for btc

--- src/whitecity/util/btc.clj
+++ src/whitecity/util/btc.clj
@@ -2,6 +2,8 @@
   (:require
    [clj-btc.core :as btc]))
 
+(defonce digits58 "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
+
 (def config {:rpcpassword "whitecity"
              :rpcuser "devil"
              :rpchost "http://127.0.0.1"
@@ -13,4 +15,19 @@
 (defn newaddress [account]
   (btc/getnewaddress :account (str account) :config config))
 
-(defn validate [address] address)
+(defn decode-base58 [s]
+  (byte-array
+    (cons 0
+        (seq 
+          (.toByteArray
+            (.toBigInteger (reduce #(+ (* 58 %) %2) (map #(bigint (.indexOf digits58 (str %))) s))))))))
+
+(defn validate [bc]
+  (let [bcbytes (decode-base58 bc)
+        md (java.security.MessageDigest/getInstance "SHA-256")
+        hashone (do (.update md (byte-array (drop-last 4 bcbytes))) (.digest md))
+        md (java.security.MessageDigest/getInstance "SHA-256")
+        hashtwo (do (.update md hashone) (.digest md))]
+    (= 
+      (take-last 4 bcbytes) 
+      (take 4 hashtwo))))
