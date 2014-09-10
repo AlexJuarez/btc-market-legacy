cf1c3cd4b2e3f4c0e9e98415ce29f3e9001468dc
Alexander Juarez <couatli@gmail.com>
2014-09-09 18:34:33 -0700

    working on the cart

--- src/whitecity/routes/cart.clj
+++ src/whitecity/routes/cart.clj
@@ -38,14 +38,13 @@
 (defn prep-postages [postages]
   (apply merge (map #(hash-map (:id %) (:price %)) postages)))
 
-(defn prep-listing [{:keys [price lid] :as listing} postages updates]
+(defn prep-listing [{:keys [price lid] :as listing} postages error-hash]
   (let [quantity (or (cart-get lid :quantity) 0)
         postage (or (postages (cart-get lid :postage)) 0)
         subtotal (* price quantity)
         total (+ subtotal postage)
-        errors (or (get (:errors updates) lid) {})
-        new (or (get (:cart updates) lid) {})]
-    (assoc listing :subtotal subtotal :total total :errors errors :new new)))
+        errors (or (get error-hash lid) {})]
+    (assoc listing :subtotal subtotal :total total :errors errors)))
 
 (defn prep-listings [listings updates]
   (let [postages (apply merge (map #(prep-postages (:postage %)) listings))]
@@ -55,35 +54,26 @@
   (session/put! :cart {})
   (resp/redirect "/cart"))
 
-(defn cart-update [{:keys [quantity postage]}]
-  (let [quantities (reduce-kv #(assoc % (util/parse-int %2) {:max (maxes (util/parse-int %2)) :quantity (or (util/parse-int %3) %3)}) {} quantity)
-        postages (reduce-kv #(assoc % (util/parse-int %2) {:postage (or (util/parse-int %3) %3)}) {} postage)
-        cart-changes (merge-with merge quantities postages)]
-    (let [cart  (apply dissoc (merge-with merge (session/get :cart) new-cart) (keep #(if (>= 0 (:quantity (val %))) (key %)) cart))]
-      (session/put! :cart cart)
-      cart)))
-
 (defn cart-update [{:keys [quantity postage]} listings]
   (let [maxes (reduce merge (map #(hash-map (:lid %) (:quantity %)) listings))
         quantities (reduce-kv #(assoc % (util/parse-int %2) {:max (maxes (util/parse-int %2)) :quantity (or (util/parse-int %3) %3)}) {} quantity)
         postages (reduce-kv #(assoc % (util/parse-int %2) {:postage (or (util/parse-int %3) %3)}) {} postage)
-        new-cart (merge-with merge quantities postages)
-        errors (reduce-kv #(let [e (v/cart-item-validator %3)] (when-not (empty? e) (assoc % %2 e))) {} new-cart)]
-    (if (empty? errors)
-      (let [cart  (merge-with merge (session/get :cart) new-cart)]
-        (session/put! :cart (apply dissoc cart (keep #(if (>= 0 (:quantity (val %))) (key %)) cart))))
-      {:errors errors :cart new-cart})))
+        cart-changes (merge-with merge quantities postages)]
+    (let [cart (merge-with merge (session/get :cart) cart-changes)
+          cart (apply dissoc cart (keep #(if (>= 0 (:quantity (val %))) (key %)) cart))]
+      (session/put! :cart cart)
+      cart)))
 
 (defn get-listings [slug]
   (let [ls (listing/get-in (keys (session/get :cart)))
-        updates (when-not (empty? slug) (cart-update (first slug) ls))
-        cart (session/get :cart)
+        cart (cart-update slug ls)
         ls (keep #(if (not (nil? (get cart (:lid %)))) %) ls)
-        listings (prep-listings ls updates)]
-    {:listings listings :errors (:errors updates)}))
+        errors (reduce-kv #(let [e (v/cart-item-validator %3)] (when-not (empty? e) (assoc % %2 e))) {} cart)
+        listings (prep-listings ls errors)]
+    listings))
 
 (defn cart-view [& slug]
-  (let [listings (:listings (get-listings slug))
+  (let [listings (get-listings (first slug))
         total (reduce + (map #(:total %) listings))
         btc-total (util/convert-price (:currency_id (util/current-user)) 1 total)]
     (layout/render "users/cart.html" (merge {:errors {}
@@ -96,12 +86,10 @@
   (if (= "Update Cart" submit)
     (cart-view slug)
     (let [listings (get-listings slug)
-          errors (:errors listings)
-          listings (:listings listings)
           total (reduce + (map #(:total %) listings))
           btc-total (util/convert-price (:currency_id (util/current-user)) 1 total)
           order (orders/add! (session/get :cart) btc-total address pin (user-id))]
-      (if (and (empty? (:errors order)) (empty? errors))
+      (if (empty? (:errors order))
         (resp/redirect "/orders")
         (layout/render "users/cart.html" (merge {:errors {}
                                                  :convert (not (= (:currency_id (util/current-user)) 1))
